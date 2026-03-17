ffa4f57719a0e1e8a92b381123044d263513e5b6
Scetrov <git@scetrov.live>
2026-03-17 21:01:51 +0000

    chore: add pre-commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 const appVersion = process.env.npm_package_version ?? "0.0.0";
+const basePath = process.env.VITE_BASE_PATH ?? "/";
 
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(appVersion),
